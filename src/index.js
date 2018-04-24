9ba0dd488b3a1b03930225703a2e41f98d74e969
Trevor Jarrett <tjarrett@trendyminds.com>
2018-04-24 16:01:31 -0400

    Refactoring of Placeholder Logic
    After a conversation with Aaron the placeholder on hover was too much.
    
    - Removed events
    - Removed show and hide placeholder functions.
    - added $el.placeholder = 'placeholder format' to the phone numbers and date fields. This will cause the placeholders to be set and display on page load.

--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ export default class TMIValidation {
     // Kick off the instantiation process for the form
     this.initializeValidation();
     this.setupCustomRules();
-    this.events();
   }
 
   forEach(array, callback, scope) {
@@ -73,51 +72,6 @@ export default class TMIValidation {
     this.forEach(this.$fieldLimit, (i, el) => this.validateFieldLimit(el));
     this.forEach(this.$checkboxGroup, (i, el) => this.validateCheckboxGroup(el));
     this.forEach(this.$dupNumCheck, (i, el) => this.validateDupNum(el));
-  }
-
-  events() {
-    this.dates = this.date = $('[data-validate-birthdate]');
-    this.forEach(this.dates, (i, el) => this.clearPlaceholder(el));
-
-    this.$phone.addEventListener('mouseover', this.showPlaceholder);
-    this.$phone.addEventListener('mouseout', this.removePlaceholder);
-    this.$phone.addEventListener('focus', this.showPlaceholder);
-    this.$phone.addEventListener('blur', this.removePlaceholder);
-
-    this.$optionalPhone.addEventListener('mouseover', this.showPlaceholder);
-    this.$optionalPhone.addEventListener('mouseout', this.removePlaceholder);
-    this.$optionalPhone.addEventListener('focus', this.showPlaceholder);
-    this.$optionalPhone.addEventListener('blur', this.removePlaceholder);
-
-    this.$birthdate.addEventListener('mouseover', this.showPlaceholder);
-    this.$birthdate.addEventListener('mouseout', this.removePlaceholder);
-    this.$birthdate.addEventListener('focus', this.showPlaceholder);
-    this.$birthdate.addEventListener('blur', this.removePlaceholder);
-
-    this.$date.addEventListener('mouseover', this.showPlaceholder);
-    this.$date.addEventListener('mouseout', this.removePlaceholder);
-    this.$date.addEventListener('focus', this.showPlaceholder);
-    this.$date.addEventListener('blur', this.removePlaceholder);
-  }
-
-  clearPlaceholder(el) {
-    el.setAttribute('placeholder', '');
-  }
-
-  showPlaceholder(e) {
-    if (e.target.hasAttribute('data-validate-phone')) {
-        if (!e.target.getAttribute('placeholder') || e.target.getAttribute('placeholder') === '') {
-          e.target.setAttribute('placeholder', '(___) ___-____');
-        }
-    } else if (e.target.hasAttribute('data-validate-birthdate')) {
-      if (!e.target.getAttribute('placeholder') || e.target.getAttribute('placeholder') === '') {
-        e.target.setAttribute('placeholder', '__/__/____');
-      }
-    }
-  }
-
-  removePlaceholder(e) {
-    e.target.setAttribute('placeholder', '');
   }
 
   validateFirstName($el) {
@@ -287,6 +241,8 @@ export default class TMIValidation {
       }
     );
 
+    $el.placeholder = '(___) ___-____';
+
     maskInput({
       inputElement: $el,
       mask: ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/],
@@ -332,6 +288,8 @@ export default class TMIValidation {
       }
     );
 
+    $el.placeholder = '(___) ___-____';
+
     maskInput({
       inputElement: $el,
       mask: ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/],
@@ -355,6 +313,8 @@ export default class TMIValidation {
       }
     );
 
+    $el.placeholder = '__/__/____';
+
     maskInput({
       inputElement: $el,
       mask: [/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/],
@@ -377,6 +337,8 @@ export default class TMIValidation {
         return valid;
       }
     );
+
+    $el.placeholder = '__/__/____';
 
     maskInput({
       inputElement: $el,
